95b90afa24e18a0634e8136052283bec4fad8949
unknown <memorytemp5@gmail.com>
2023-09-26 19:03:12 +0530

    Add Sms And One Divice Login

---
+++ sms_otp/app/Http/Controllers/SendSMSController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Twilio\Rest\Client;
+
+class SendSMSController extends Controller
+{
+    public function sendSms(Request $request)
+    {
+        $sid = env('TWILIO_SID');
+        $token = env('TWILIO_AUTH_TOKEN');
+        $twilioPhoneNumber = env('TWILIO_PHONE_NUMBER');
+        $client = new Client($sid, $token);
+
+        $toPhoneNumber = "+917002142698"; // Replace with the recipient's phone number
+        $message = "Hellow"; // Replace with the message you want to send
+
+        $client->messages->create(
+            $toPhoneNumber,
+            [
+                'from' => $twilioPhoneNumber,
+                'body' => $message,
+            ]
+        );
+
+        return "SMS sent successfully!";
+    }
+}
